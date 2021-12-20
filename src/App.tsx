16df764844ac5654eac28f908c1c221c947cf5a8
QAQmmttyyy <miaotianyugo@163.com>
2021-12-20 20:38:41 +0800

    调整导入文件路径

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Toolbar from "./UI/Toolbar";
-import Viewer from "./UI/Viewer";
+import Toolbar from "./ui/Toolbar";
+import Viewer from "./ui/Viewer";
 import "./styles.scss";
 
 export default function App() {
